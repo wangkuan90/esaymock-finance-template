7dab2d743fae4c06395a9bd77b843f48fd1ca131
wangkuan <wangkuan@souche.com>
2017-07-22 19:46:08 +0800

    add

--- template/cover/index.js
+++ template/cover/index.js
@@ -1,14 +1,19 @@
+/* eslint-disable */
 import instance from './instance';
 
-<% _.forEach(data.mocks, function(mock){ %>/** <%- mock.description %> */
-function <%- $$.convertUrl(mock.url) %>(opts) {
-  return instance({
-    method: '<%- mock.method %>',
-    url: '<%- mock.url %>',
-    opts: opts
-  });
+<% _.forEach(data.mocks, function(mock){ %> /** <%- mock.description %> */
+function <%- $$.convertUrl(mock.url) %> (opts) {
+    return instance({
+        method: '<%- mock.method %>',
+        url: '<%- mock.url %>',
+        opts: opts
+    });
 }
 
-<% }) %>export {<% _.forEach(data.mocks, function(mock, i){ %>
-  <%- $$.convertUrl(mock.url) %><% if(data.mocks.length - 1 !== i) { %>,<% } %><% }) %>
+<% }) %>
+export {
+    <% _.forEach(data.mocks, function(mock, i){ %>
+    <%- $$.convertUrl(mock.url) %>
+    <% if(data.mocks.length - 1 !== i) { %>, <% } %>
+    <% }) %>
 };
